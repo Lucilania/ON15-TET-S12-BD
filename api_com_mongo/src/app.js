e12decf562972965a3e3d34a850a6ee4aaa8c511
lucilania <lucilaniasilva@yahoo.com.br>
2022-07-01 16:20:49 -0300

    alteração

--- api_com_mongo/src/app.js
+++ api_com_mongo/src/app.js
@@ -1,1 +1,14 @@
+const express = require('express')
+require('dotenv').config()
+const cors = require('cors')
 
+const db = require('./database/mongooseConect')
+
+
+const app = express()
+
+app.use(express.joson())
+app.use(cors())
+db.connect()
+
+module.exports = app
---
+++ api_com_mongo/src/models/musicaModel.js
@@ -0,0 +1,6 @@
+const mongoose  = require('mongoose')
+
+const MusicaSchema = mongoose.Schema({
+    _id: mongoose.Types.ObjectID
+    
+})
